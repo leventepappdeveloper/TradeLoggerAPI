deabff85b36d596acc2562d59186239a3adb11a7
papplevi96 <Realmadrid96>
2021-04-22 20:19:31 -0700

    set up database connection

--- TradeLoggerAPIProject/settings.py
+++ TradeLoggerAPIProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TradeLoggerAPIProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bkgwshrxolshvlsyc1qj',
+        'USER': 'uhfrvlpwnsoobwfz',
+        'PASSWORD': '6EX4fQ7O1b1KGm6MR3UU',
+        'HOST': 'bkgwshrxolshvlsyc1qj-mysql.services.clever-cloud.com',
+        'PORT': '3306',
     }
 }
 

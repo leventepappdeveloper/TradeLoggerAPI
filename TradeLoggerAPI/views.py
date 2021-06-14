270442776403cc3b3cc670daa51e0905330236ce
papplevi96 <Realmadrid96>
2021-06-13 19:18:33 -0700

    Initial implementation for Swagger UI added

--- TradeLoggerAPI/views.py
+++ TradeLoggerAPI/views.py
@@ -5,10 +5,18 @@ from .serializers import UserSerializer
 from rest_framework.response import Response
 from .models import User
 import jwt, datetime
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 # Create your views here.
 # extends APIView that has post and get methods
 class RegisterView(APIView):
+    registerSwaggerSchemaParameters = {'name': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+                                        'email': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+                                        'password': openapi.Schema(type=openapi.TYPE_STRING, description='string')}
+    registerSwaggerSchema = openapi.Schema(type=openapi.TYPE_OBJECT, properties=registerSwaggerSchemaParameters)
+
+    @swagger_auto_schema(operation_description="description", request_body=registerSwaggerSchema)
     def post(self, request):
         # serializes the fields that we want to return in the response
         # adds data to database, too?
@@ -19,53 +27,50 @@ class RegisterView(APIView):
 
 
 class LoginView(APIView):
+    loginSwaggerSchemaParameters = {'email': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+                                    'password': openapi.Schema(type=openapi.TYPE_STRING, description='string')}
+    loginSwaggerSchema = openapi.Schema(type=openapi.TYPE_OBJECT, properties=loginSwaggerSchemaParameters)
+
+    @swagger_auto_schema(operation_description="description", request_body=loginSwaggerSchema)
     def post(self, request):
         email = request.data['email']
         password = request.data['password']
         user = User.objects.filter(email=email).first()
 
-        response = Response()
-        response["Access-Control-Expose-Headers"] = '*'
-        token = ''
-        response["Authorization"] = "jwt=" + token;
-
         if user is None:
-            #raise AuthenticationFailed('User not found!')
-            return response
-
+            raise AuthenticationFailed('User not found!')
         if not user.check_password(password):
-            #raise AuthenticationFailed('Incorrect password!')
-            return response
+            raise AuthenticationFailed('Incorrect password!')
 
+        # Build JWT Token - valid for 60 seconds
         payload = {
             'id': user.id,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
             'iat': datetime.datetime.utcnow()
         }
-
-        # secret will need to go somewhere else
         token = jwt.encode(payload, 'secret', algorithm="HS256")
 
-        # look into httponly here
-        response.set_cookie(key='TradeLoggerAuthCookie', value=token, httponly=True)
-        response["Authorization"] = "jwt=" + token
+        # Add Bearer token auth header to response message
+        response = Response()
+        response["Authorization"] = "Bearer " + token
 
         return response
 
 class UserView(APIView):
-    # gets cookie and retrieves user corresponding to token in cookie
     def get(self, request):
-        token = request.COOKIES.get('jwt')
-        # token cookie to get user
+        token = request.META.get('HTTP_AUTHORIZATION')
         if not token:
-            raise AuthenticationFailed('Unauthenticated')
+            raise AuthenticationFailed('No authorization token found. Unauthenticated!')
 
         try:
-            # see payload above
+            # remove "BEARER " part from token, and get its payload
+            token = token[7:]
             payload = jwt.decode(token, 'secret', algorithms=["HS256"])
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('Unauthenticated!')
+        except:
+            raise AuthenticationFailed('Invalid authorization token provided. Unauthenticated!')
 
+        # find User object in the db corresponding to the "id" found
+        # in the JWT token (and serialize JSON User object)
         user = User.objects.filter(id=payload['id']).first()
         serializer = UserSerializer(user)
 
